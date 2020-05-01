4e9698f398dc4952c9f95f3d78d9ef27c51757a0
LMField82 <meneslindsay@hotmail.com>
2020-05-01 14:51:11 -0600

    adding page content for Projects

---
+++ lmf-portfolio/src/Pages/projects.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import ProjectCard from "../components/ProjectCards";
+import cards from "../cards.json";
+
+ 
+// const handleClick = (event) => {
+//     event.preventDefault();
+    
+//  }
+
+    class Projects extends Component {
+    state = {
+      cards
+    };
+
+render()  {
+   
+    return (
+        <div className="row">
+            {this.state.cards.map(cards => (
+                
+            <ProjectCard 
+                key={cards.id}
+                id={cards.id}
+                image={cards.image}
+                title={cards.title}
+                text={cards.text}
+                btnText={cards.btnText}
+                link={cards.link}
+            />
+            ))}
+        </div>
+      )
+  
+    }
+};
+
+export default Projects;
