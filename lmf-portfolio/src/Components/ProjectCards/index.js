c1c45a0feb5a5460c06d54e682555fa183c4685a
LMField82 <meneslindsay@hotmail.com>
2020-05-01 14:42:25 -0600

    adding ProjectCards Component content

---
+++ lmf-portfolio/src/Components/ProjectCards/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import "../styles/projects.css";
+// import { Container, Row, Col } from "./Layout"
+
+function ProjectCard(props) {
+  return (
+    
+        <div className="col-sm-4" >
+    <div id="contForCards">
+        <div className="card">
+            
+            <img src={props.image} className="card-img-top" alt={props.title} />
+               
+                
+            <div className="card-body">
+                <h4 className="card-title">{props.title}</h4>
+                <p className="card-text">{props.text}</p>
+                <a id="proj-links" target="blank" href={props.link}>{props.btnText}</a>
+            </div>
+         </div>      
+            
+        </div>
+       </div>
+     
+   
+  );
+}
+
+export default ProjectCard;
+
