e89b45f01f42431785fe2206fb27ed0cd8f14572
LMField82 <meneslindsay@hotmail.com>
2020-05-01 13:21:32 -0600

    added card component

---
+++ lmf-portfolio/src/Components/Card/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Card(props) {
+  return (
+    <div className="card-container">
+        <div className="card">
+            <div>
+                <img src={props.image} className="card-img-top" alt={props.title} />
+        
+                <div className="card-body">
+                    <h4 className="card-title">{props.title}</h4>
+                    <p className="card-text">{props.text}</p>
+                    <button href={props.link}>{props.btnText}</button>
+                </div>
+               
+            </div>
+        </div>
+    </div>
+
+  );
+}
+
+export default Card;
