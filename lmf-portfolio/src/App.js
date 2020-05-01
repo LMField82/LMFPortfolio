ddae5c0389a670b8d3ce9241eedde090e4a11b08
LMField82 <meneslindsay@hotmail.com>
2020-05-01 15:05:00 -0600

    adding import statement

--- lmf-portfolio/src/App.js
+++ lmf-portfolio/src/App.js
@@ -3,6 +3,7 @@ import Nav from './Components/Nav';
 import Footer from './Components/Footer';
 import About from './Pages/about';
 import Projects from './Pages/projects';
+import Contact from './Pages/contact';
 // import logo from './logo.svg';
 import './App.css';
 
