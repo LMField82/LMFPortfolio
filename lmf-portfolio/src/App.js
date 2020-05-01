75f192cfb50e42c7175aa884d5dce2da144a00f3
LMField82 <meneslindsay@hotmail.com>
2020-04-30 21:43:12 -0600

    adding Footer

--- lmf-portfolio/src/App.js
+++ lmf-portfolio/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import Nav from './Components/Nav'
+import Nav from './Components/Nav';
+import Footer from './Components/Footer';
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <Nav />
+      <Footer />
     </div>
   );
 }
